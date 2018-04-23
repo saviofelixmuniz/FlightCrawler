7d2810e298b0b8d3eb4e7d244cfa17cb8c7909db
Sávio Muniz <saviofelixmuniz@gmail.com>
2018-04-23 15:17:22 -0300

    cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors')
 var app = express();
 const bodyParser = require('body-parser');
 
@@ -6,6 +7,7 @@ require('./app/db/db');
 
 const routes = require('./app/routes/index');
 
+app.use(cors());
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ limit: '1mb', extended: false }));
 app.use('/api', routes);
