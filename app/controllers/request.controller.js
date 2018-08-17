1f288e2d38d0b65a5400d3a1db79f5520d50c5ae
Savio Muniz <savio.muniz@ccc.ufcg.edu.br>
2018-08-17 15:15:56 -0300

    [ISSUE] 404 para ids inválidos

--- app/controllers/request.controller.js
+++ app/controllers/request.controller.js
@@ -3,18 +3,27 @@ var Requests = require('../db/models/requests');
 function getParams(req, res, next) {
     var id = req.params.id;
     Requests.findOne({_id: id}).then(function (obj) {
+        if (!obj){
+            res.status(404).json({err: 'id is invalid'});
+            return;
+        }
+
         res.status(200).json(obj.params);
     }).catch(function (err) {
-        res.status(400).json(res, 400, err);
+        res.status(400).json(err);
     });
 }
 
 function getRequest(req, res, next) {
     var id = req.params.id;
-    Requests.findOne({_id: id}).then(function (obj) {
+    Requests.findOne({_id: id}).then(function (obj)  {
+        if (!obj) {
+            res.status(404).json({err: 'id is invalid'});
+            return;
+        }
         res.status(200).json(obj);
     }).catch(function (err) {
-        res.status(400).json(res, 400, err);
+        res.status(400).json(err);
     });
 }
 
