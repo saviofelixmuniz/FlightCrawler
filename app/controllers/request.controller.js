33f32819198eef678498495e4600b4c9f95eca27
samkya fernandes <samkyafernandes@MacBook-Pro-de-samkya.local>
2018-09-08 08:26:00 -0300

    [FEATURE] Pesquisa em banco feita

--- app/controllers/request.controller.js
+++ app/controllers/request.controller.js
@@ -27,7 +27,27 @@ function getRequest(req, res, next) {
     });
 }
 
+function getFlight(req, res, next) {
+    var flightId = req.params.id;
+    Requests.$where(`for (var trecho in this.response['Trechos']) {for (var flight of this.response['Trechos'][trecho].Voos) {if (flight._id == '${flightId}') {return true;}}}return false;`).exec(function (oi, tchau) {
+        console.log(tchau);
+    })
+}
+
+function matchFlight(){
+    console.log(this);
+    for (var trecho in this.response['Trechos']) {
+        for (var flight of this.response['Trechos'][trecho].Voos) {
+            if (flight.id == "5b9327399649f2139066335e") {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 module.exports = {
     getRequest: getRequest,
-    getRequestParams: getParams
+    getRequestParams: getParams,
+    getFlight: getFlight
 };
